430018884a86d40d50e0b396e9f49ee53cb87cac
Sagar-Gopalasetti <gopalasettisagar@gmail.com>
2025-02-01 13:45:05 -0500

    push test

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/sagargopalasetti/Desktop/Naresh React/remotion-test/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/sagargopalasetti/Desktop/Naresh React/remotion-test/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/sagargopalasetti/Desktop/Naresh React/remotion-test/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/sagargopalasetti/Desktop/Naresh React/remotion-test/src/css/custom.css"),
+];
